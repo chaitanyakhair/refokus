43997e9dabc54e76816451f1610b0e28f63353ec
chaitanyakhair <chaitanyakhair@gmail.com>
2024-04-25 22:35:33 +0530

    frontend done

--- refokus/src/components/Card.jsx
+++ refokus/src/components/Card.jsx
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion';
 import React from 'react'
 import { FaArrowRightLong } from "react-icons/fa6";
 
+
 function Card({width , start , para , hover}) {
 
   return (
@@ -18,7 +19,9 @@ function Card({width , start , para , hover}) {
             start && (
                 <>
                     <h1 className='text-6xl font-semibold tracking-normal leading-none'>start a project</h1>
-                    <button className='rounded-full mt-5 py-2 px-5 border-[1px] border-zinc-100'>Contact us</button>
+ 
+                    <button  className='rounded-full mt-5 py-2 px-5 border-[1px] border-zinc-100'>Contact us</button>
+
                 </>
             )
         }
--- refokus/src/components/Products.jsx
+++ refokus/src/components/Products.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Product from './Product';
 import { motion } from 'framer-motion';
 
+
 function Products() {
 
     var products = [
@@ -20,11 +21,31 @@ function Products() {
     <div className='mt-20 bg-zinc-900 relative'>
         {products.map((val, index) => <Product val={val} mover={mover} count={index} />)}
         <div className='absolute top-0 w-full h-full pointer-events-none'>
-          <motion.div initial={{ y: pos, x : "-50%" }} animate={{y: pos + `rem`}} transition={{ease: [0.76, 0, 0.24, 1], duration: 0.6}} className='window absolute w-[32rem] h-[20rem] bg-sky-100 left-[44%] overflow-hidden'>
-            <motion.div animate={{ y: -pos+`rem`}} transition={{ease: [0.76, 0, 0.24, 1], duration: 0.5}} className=' w-full h-full bg-sky-400'></motion.div>
-            <motion.div animate={{ y: -pos+`rem`}} transition={{ease: [0.76, 0, 0.24, 1], duration: 0.5}} className=' w-full h-full bg-sky-600'></motion.div>
-            <motion.div animate={{ y: -pos+`rem`}} transition={{ease: [0.76, 0, 0.24, 1], duration: 0.5}} className=' w-full h-full bg-sky-800'></motion.div>
-            <motion.div animate={{ y: -pos+`rem`}} transition={{ease: [0.76, 0, 0.24, 1], duration: 0.5}} className=' w-full h-full bg-sky-900'></motion.div>
+          <motion.div initial={{ y: pos, x : "-50%" }} animate={{y: pos + `rem`}} transition={{ease: [0.76, 0, 0.24, 1], duration: 0.6}} className='window absolute w-[32rem] h-[20rem] bg-sky-100 left-[44%] overflow-hidden rounded-2xl'>
+            <motion.div 
+              animate={{ y: -pos+`rem`}} 
+              transition={{ease: [0.76, 0, 0.24, 1], duration: 0.5}}  
+              className=' w-full h-full bg-sky-400'>
+              <img src='/src/assets/1.gif' className=' object-cover -translate-y-8'/> 
+              </motion.div>
+            <motion.div 
+              animate={{ y: -pos+`rem`}} 
+              transition={{ease: [0.76, 0, 0.24, 1], duration: 0.5}} 
+              className=' w-full h-full bg-sky-600'>
+                <img src='/src/assets/2(1)(1).gif' className=' scale-100 '/>
+              </motion.div>
+            <motion.div 
+              animate={{ y: -pos+`rem`}} 
+              transition={{ease: [0.76, 0, 0.24, 1], duration: 0.5}} 
+              className=' w-full h-full bg-sky-800'>
+              <img src='/src/assets/3.gif' className=' scale-100 '/>
+              </motion.div>
+            <motion.div 
+              animate={{ y: -pos+`rem`}} 
+              transition={{ease: [0.76, 0, 0.24, 1], duration: 0.5}} 
+              className=' w-full h-full bg-sky-900'>
+              <img src='/src/assets/4(1)(1).gif' className=' scale-100 '/>
+              </motion.div>
           </motion.div>
         </div>
     </div>
